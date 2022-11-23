1c0f2113ca1df429926955df3f206412eae47ad6
9663461085 <ratansollapure16310@gmail.com>
2022-11-23 10:11:34 +0530

    Added:Used swicth Case to calculate emloyee Wage

--- src/com/bridgelabzs/EmployeeWage.java
+++ src/com/bridgelabzs/EmployeeWage.java
@@ -13,6 +13,8 @@ public class EmployeeWage {
 	static final int WAGE_PER_HOUR=20;
     static final int FULL_TIME_HOUR=8;
     static final int PART_TIME_HOUR=4;
+    static final int IS_FULL_TIME=2;
+    static final int IS_PART_TIME=1;
 public static void main(String args[]) {
 	/*
 	 * Checked the employee present or absent condition by using random function
@@ -25,21 +27,18 @@ public static void main(String args[]) {
 	/*
 	 * Checking the condition by using if esle condition
 	 */
-	if(checkResult==2) {
-		System.out.println("Employee is present");
-		/*
-		 * daily wage calcultion
-		 */
-		dailyWage=WAGE_PER_HOUR*FULL_TIME_HOUR;
-	}
-	else if(checkResult==1){
-		System.out.println("Employee is part time ");
-		dailyWage=WAGE_PER_HOUR*PART_TIME_HOUR;
-	}
-	else {
-		System.out.println("Employe is absent");
-	}
-	
-	 System.out.println("Daily Wage of employee is => "+dailyWage);
+	switch(checkResult) {
+    case IS_FULL_TIME:
+        System.out.println("Employee is Full Time");
+        dailyWage = WAGE_PER_HOUR * FULL_TIME_HOUR;
+        break;
+    case IS_PART_TIME:
+        System.out.println("Employee is Part Time");
+        dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
+        break;
+    default:
+        System.out.println("Employee is absent");
+}
+System.out.println("Daily Wage of employee is => "+dailyWage);
 }
 }
