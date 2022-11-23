ff18abc30a4295978538c17f87ca78ffd9f7e630
9663461085 <ratansollapure16310@gmail.com>
2022-11-23 09:33:54 +0530

    Added Welcome Meassge to Employe wage Computation program

---
+++ src/com/bridgelabzs/EmployeeWage.java
@@ -0,0 +1,19 @@
+package com.bridgelabzs;
+/**
+ * 
+ * @author Admin
+ *
+ */
+
+public class EmployeeWage {
+/**
+ * 
+ * @param args
+ */
+public static void main(String args[]) {
+	/* 
+	 * Printing welcome meassage;
+	 */
+	System.out.println("Welcome to Employee wage Computation Program");
+}
+}
