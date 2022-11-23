de1d7548d7235496df56ebe37623745588b0699f
9663461085 <ratansollapure16310@gmail.com>
2022-11-23 10:05:39 +0530

    Added:Calculted Employee Part time Wage

--- src/com/bridgelabzs/EmployeeWage.java
+++ src/com/bridgelabzs/EmployeeWage.java
@@ -12,20 +12,34 @@ public class EmployeeWage {
  */
 	static final int WAGE_PER_HOUR=20;
     static final int FULL_TIME_HOUR=8;
+    static final int PART_TIME_HOUR=4;
 public static void main(String args[]) {
 	/*
 	 * Checked the employee present or absent condition by using random function
 	 */
-	int checkResult=(int)Math.floor(Math.random()*10) % 2;
+	int checkResult=(int)Math.floor(Math.random()*10) % 3;
+	/*
+	 * initial Daily wage we have to take zero
+	 */
 	int dailyWage=0;
-	
-	if(checkResult==1) {
+	/*
+	 * Checking the condition by using if esle condition
+	 */
+	if(checkResult==2) {
 		System.out.println("Employee is present");
+		/*
+		 * daily wage calcultion
+		 */
 		dailyWage=WAGE_PER_HOUR*FULL_TIME_HOUR;
 	}
-	else {
-		System.out.println("Employee is Absent");
+	else if(checkResult==1){
+		System.out.println("Employee is part time ");
+		dailyWage=WAGE_PER_HOUR*PART_TIME_HOUR;
 	}
+	else {
+		System.out.println("Employe is absent");
+	}
+	
 	 System.out.println("Daily Wage of employee is => "+dailyWage);
 }
 }
