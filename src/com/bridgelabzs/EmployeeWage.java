24a9225223be8f7224be50a4a5309afda1f96377
9663461085 <ratansollapure16310@gmail.com>
2022-11-23 09:56:18 +0530

    Added:Calculated Employee Daily wage

--- src/com/bridgelabzs/EmployeeWage.java
+++ src/com/bridgelabzs/EmployeeWage.java
@@ -10,16 +10,22 @@ public class EmployeeWage {
  * 
  * @param args
  */
+	static final int WAGE_PER_HOUR=20;
+    static final int FULL_TIME_HOUR=8;
 public static void main(String args[]) {
 	/*
 	 * Checked the employee present or absent condition by using random function
 	 */
 	int checkResult=(int)Math.floor(Math.random()*10) % 2;
+	int dailyWage=0;
+	
 	if(checkResult==1) {
 		System.out.println("Employee is present");
+		dailyWage=WAGE_PER_HOUR*FULL_TIME_HOUR;
 	}
 	else {
 		System.out.println("Employee is Absent");
 	}
+	 System.out.println("Daily Wage of employee is => "+dailyWage);
 }
 }
