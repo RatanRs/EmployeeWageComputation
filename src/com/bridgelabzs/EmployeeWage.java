7a099ccfa48ebe859c24322881ac41209cd31c17
9663461085 <ratansollapure16310@gmail.com>
2022-11-23 10:51:10 +0530

    Added:Calculated Employee Total working hour in a month

--- src/com/bridgelabzs/EmployeeWage.java
+++ src/com/bridgelabzs/EmployeeWage.java
@@ -1,4 +1,5 @@
 package com.bridgelabzs;
+
 /**
  * 
  * @author Admin
@@ -6,48 +7,52 @@ package com.bridgelabzs;
  */
 
 public class EmployeeWage {
-/**
- * 
- * @param args
- */
-	static final int WAGE_PER_HOUR=20;
-    static final int FULL_TIME_HOUR=8;
-    static final int PART_TIME_HOUR=4;
-    static final int IS_FULL_TIME=2;
-    static final int IS_PART_TIME=1;
-    static final int WORKING_DAYS_PER_MONTH=20;
-public static void main(String args[]) {
-	
-	
-	
-	int day=1;
-	int totalWage=0;
-	while(day<=WORKING_DAYS_PER_MONTH) {
-		/*
-		 * Checked the employee present or absent condition by using random function
-		 */
-		
-		int checkResult=(int)Math.floor(Math.random()*10) % 3;
-	int dailyWage=0;
-	/*
-	 * Checking the condition by using switch case 
+	/**
+	 * 
+	 * @param args
 	 */
-	switch(checkResult) {
-    case IS_FULL_TIME:
-        System.out.println("Employee is Full Time");
-        dailyWage = WAGE_PER_HOUR * FULL_TIME_HOUR;
-        break;
-    case IS_PART_TIME:
-        System.out.println("Employee is Part Time");
-        dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
-        break;
-    default:
-        System.out.println("Employee is absent");
-}
-System.out.println("Daily Wage of employee is => "+dailyWage);
-day++;
-}
-	 System.out.println("Tatal Wages of employee is => "+totalWage);
-}
-}
+	static final int WAGE_PER_HOUR = 20;
+	static final int FULL_TIME_HOUR = 8;
+	static final int PART_TIME_HOUR = 4;
+	static final int IS_FULL_TIME = 2;
+	static final int IS_PART_TIME = 1;
+	static final int WORKING_DAYS_PER_MONTH = 20;
+	static final int WORKING_HOURS_PER_MONTH=100;
 
+	public static void main(String args[]) {
+
+		int day = 1;
+		int totalWage = 0;
+		int employeeHours=0;
+		while(day<=WORKING_DAYS_PER_MONTH && employeeHours<=WORKING_HOURS_PER_MONTH) {
+			/*
+			 * Checked the employee present or absent condition by using random function
+			 */
+
+			int checkResult = (int) Math.floor(Math.random() * 10) % 3;
+			int dailyWage = 0;
+			/*
+			 * Checking the condition by using switch case
+			 */
+			switch (checkResult) {
+			case IS_FULL_TIME:
+				System.out.println("Employee is Full Time");
+				dailyWage = WAGE_PER_HOUR * FULL_TIME_HOUR;
+				 employeeHours=employeeHours+FULL_TIME_HOUR;
+				break;
+			case IS_PART_TIME:
+				System.out.println("Employee is Part Time");
+				dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
+				 employeeHours=employeeHours+PART_TIME_HOUR;
+				break;
+			default:
+				System.out.println("Employee is absent");
+			}
+			System.out.println("Daily Wage of employee is => " + dailyWage);
+			day++;
+		}
+		System.out.println("Total Wages of employee is => " + totalWage);
+		System.out.println("Total Wages of employee is => " + employeeHours);
+		
+	}
+}
