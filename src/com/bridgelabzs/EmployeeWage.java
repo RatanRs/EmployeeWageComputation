0e3a0aa8e70fee4f3820c2b70c2ea561a9814cc4
9663461085 <ratansollapure16310@gmail.com>
2022-11-29 21:25:39 +0530

    Added:Created Class method and variables

--- src/com/bridgelabzs/EmployeeWage.java
+++ src/com/bridgelabzs/EmployeeWage.java
@@ -17,42 +17,39 @@ public class EmployeeWage {
 	static final int IS_FULL_TIME = 2;
 	static final int IS_PART_TIME = 1;
 	static final int WORKING_DAYS_PER_MONTH = 20;
-	static final int WORKING_HOURS_PER_MONTH=100;
+	static final int WORKING_HOURS_PER_MONTH = 100;
 
-	public static void main(String args[]) {
-
+	public static void employeWage() {
 		int day = 1;
-		int totalWage = 0;
-		int employeeHours=0;
-		while(day<=WORKING_DAYS_PER_MONTH && employeeHours<=WORKING_HOURS_PER_MONTH) {
-			/*
-			 * Checked the employee present or absent condition by using random function
-			 */
-
-			int checkResult = (int) Math.floor(Math.random() * 10) % 3;
+		int totalWages = 0;
+		int empHours = 0;
+		while (day <= WORKING_DAYS_PER_MONTH && empHours <= WORKING_HOURS_PER_MONTH) {
+			int employeeAttendance = (int) Math.floor(Math.random() * 10) % 3;
 			int dailyWage = 0;
-			/*
-			 * Checking the condition by using switch case
-			 */
-			switch (checkResult) {
+			switch (employeeAttendance) {
 			case IS_FULL_TIME:
 				System.out.println("Employee is Full Time");
 				dailyWage = WAGE_PER_HOUR * FULL_TIME_HOUR;
-				 employeeHours=employeeHours+FULL_TIME_HOUR;
+				empHours = empHours + FULL_TIME_HOUR;
 				break;
 			case IS_PART_TIME:
 				System.out.println("Employee is Part Time");
 				dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
-				 employeeHours=employeeHours+PART_TIME_HOUR;
+				empHours = empHours + PART_TIME_HOUR;
 				break;
 			default:
 				System.out.println("Employee is absent");
 			}
-			System.out.println("Daily Wage of employee is => " + dailyWage);
+			totalWages += dailyWage;
+			System.out.println("Daily Wage of employee for day " + day + " is => " + dailyWage);
 			day++;
 		}
-		System.out.println("Total Wages of employee is => " + totalWage);
-		System.out.println("Total Wages of employee is => " + employeeHours);
-		
+		System.out.println("Total working hours of employee is => " + empHours);
+		System.out.println("Tatal Wages of employee is => " + totalWages);
 	}
+
+	public static void main(String[] args) {
+		employeWage();
+	}
+
 }
